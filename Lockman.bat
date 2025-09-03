1bd65f73f73a960ac6b87a3fd012d80bfc758a4a
Goil360 <goilbuisnesser@gmail.com>
2025-09-03 16:17:27 +0530

    Create Lockman.bat

---
+++ Lockman.bat
@@ -0,0 +1,43 @@
+@echo off
+title Lockman Folder Locker
+color 0a
+
+set "PASSWORD=password"  :: Default password is 'password'
+
+:MENU
+cls
+echo ================================
+echo        DIGILOCKER SYSTEM
+echo ================================
+echo.
+echo [1] Lock Folder
+echo [2] Unlock Folder
+echo [3] Exit
+echo.
+set /p option=Choose an option (1-3): 
+
+if "%option%"=="1" goto LOCK
+if "%option%"=="2" goto UNLOCK
+if "%option%"=="3" exit
+goto MENU
+
+:LOCK
+if not exist "Private" (
+    mkdir Private
+    echo Folder created: Private
+)
+attrib +h +s "Private"
+echo Folder is now locked
+pause
+goto MENU
+
+:UNLOCK
+set /p pass=Enter password: 
+if "%pass%"=="%PASSWORD%" (
+    attrib -h -s "Private"
+    echo Folder is now unlocked
+) else (
+    echo Wrong password
+)
+pause
+goto MENU
